f10ee05d0a595aa007423d3996cef3456c047fd7
shawnohn <developer.ohn@gmail.com>
2020-09-22 12:30:43 -0400

    init

--- server/index.js
+++ server/index.js
@@ -1,35 +1,25 @@
+require('dotenv')
 const express = require('express')
-const app = express()
-const path = require('path')
 const cors = require('cors')
+const { join } = require('path')
 
-// const bodyParser = require('body-parser')
-// const cookieParser = require('cookie-parser')
-
-// const config = require('./config/key')
+const app = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(express.static(join(__dirname, 'public')))
 
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(bodyParser.json())
-// app.use(cookieParser())
-
-// app.use('/api/users', require('./routes/users'))
-// app.use('/api/product', require('./routes/product'))
-
-// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'))
-
-  // index.html for all page routes
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
-  })
+  app.use(express.static(join(__dirname, '../client/build')))
 }
 
-const port = process.env.PORT || 5000
-
-app.listen(port, () => {
-  console.log(`Server Running at ${port}`)
+app.get('/*', function (req, res) {
+  res.sendFile(join(__dirname, '../client/build/index.html'))
 })
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server listening at ${port}`)
+})
+
+module.exports = app
