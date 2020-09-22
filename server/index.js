6db4277f02e69bf7d3074fcf418387ac9b7e50dc
shawnohn <developer.ohn@gmail.com>
2020-09-22 11:26:00 -0400

    connection heroku

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -6,6 +8,23 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000')
+/*
+ * In production mode we will also serve the client
+ */
+if (process.env.NODE_ENV === 'production') {
+  console.log(`Production mode detected: Serving client`)
+  const path = require('path')
+
+  const buildDir = path.join(__dirname, '../client/build')
+
+  app.use(express.static(buildDir))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(buildDir, 'index.html'))
+  })
+}
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log(`Server listening at ${port}`)
 })
