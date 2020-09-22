b86dc0838eb68f5beff80484a44f7f3bb01ad813
shawnohn <developer.ohn@gmail.com>
2020-09-22 11:57:18 -0400

    init

--- server/index.js
+++ server/index.js
@@ -1,30 +1,35 @@
-require('dotenv').config()
-
 const express = require('express')
 const app = express()
+const path = require('path')
 const cors = require('cors')
 
-//middleware
+// const bodyParser = require('body-parser')
+// const cookieParser = require('cookie-parser')
+
+// const config = require('./config/key')
+
 app.use(cors())
-app.use(express.json())
 
-/*
- * In production mode we will also serve the client
- */
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
+// app.use(cookieParser())
+
+// app.use('/api/users', require('./routes/users'))
+// app.use('/api/product', require('./routes/product'))
+
+// Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-  console.log(`Production mode detected: Serving client`)
-  const path = require('path')
+  // Set static folder
+  app.use(express.static('client/build'))
 
-  const buildDir = path.join(__dirname, '../client/build')
-
-  app.use(express.static(buildDir))
-
+  // index.html for all page routes
   app.get('*', (req, res) => {
-    res.sendFile(path.join(buildDir, 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'))
   })
 }
 
 const port = process.env.PORT || 5000
+
 app.listen(port, () => {
-  console.log(`Server listening at ${port}`)
+  console.log(`Server Running at ${port}`)
 })
